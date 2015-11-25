befc6ba9213e9b13e2212462cfe9e96f6a59c7b0
Tudor Dragan <me@tudordev.com>
2015-11-25 21:15:38 +0100

    first commit

---
+++ tattoodo-demo/Pods/Target Support Files/Pods-tattoodo-demo/Pods-tattoodo-demo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_tattoodo_demoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_tattoodo_demoVersionString[];
+
---
+++ tattoodo-demo/Pods/Target Support Files/Pods-tattoodo-demoTests/Pods-tattoodo-demoTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_tattoodo_demoTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_tattoodo_demoTestsVersionString[];
+
